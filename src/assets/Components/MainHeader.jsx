a3f285fc3d6fbd48a98f37384a7b92e17e551f0b
Golvan98 <gilvinsz@gmail.com>
2025-02-04 20:43:32 +0800

    yeye

---
+++ src/assets/Components/MainHeader.jsx
@@ -0,0 +1,25 @@
+import classes from './MainHeader.module.css'
+
+
+function MainHeader ()
+{
+
+
+    return (
+
+        <>
+            <header className="text-3xl font-bold underline bg-red-300 w-full">
+                <h1> Hello, Gilvin here </h1>
+<<<<<<< HEAD
+
+=======
+               testtttyawaadsaas
+>>>>>>> e9d52be (reup)
+            </header>
+        </>
+
+
+    );
+}
+
+export default MainHeader;
