b2ecc1130fb72cf9872d5e5b6f95fc6066e44856
Golvan98 <gilvinsz@gmail.com>
2025-02-05 18:58:15 +0800

    2nd day commit test

--- src/assets/Components/MainHeader.jsx
+++ src/assets/Components/MainHeader.jsx
@@ -1,25 +1,23 @@
-import classes from './MainHeader.module.css'
+import classes from './MainHeader.module.css';
+
+function MainHeader() {
+  return (
 
 
-function MainHeader ()
-{
+    <div className="fixed inset-0 bg-deepPurple">
+     
+      <header className="text-3xl font-bold flex justify-between p-3">
+        <h1 className="text-lightCyan items-center ">Gilvin🧑‍💻</h1>
+        <h1 className="text-lightCyan flex justify-betweenitems-center ">☰</h1>
+      </header>
 
+      {/* Full-Screen Section */}
+      <div id="section1" className="h-screen flex items-center justify-center bg-deepPurple text-white">
+        <h2 className="text-4xl">Section 1</h2>
+      </div>
 
-    return (
-
-        <>
-            <header className="text-3xl font-bold underline bg-red-300 w-full">
-                <h1> Hello, Gilvin here </h1>
-<<<<<<< HEAD
-
-=======
-               testtttyawaadsaas
->>>>>>> e9d52be (reup)
-            </header>
-        </>
-
-
-    );
+    </div>
+  );
 }
 
 export default MainHeader;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: 
+      {
+        deepPurple: "#1A1A2E",
+        neonPink: "#FF007F",
+        electricYellow: "#FFD700",
+        brightMagenta: "#E100FF", 
+        darkNavy:"#0A192F",
+        lightGray: "#E6E6E6",
+        lightCyan: "#00E5FF",
+    
+      },
+    },
   },
   plugins: [],
 }
