cdcf05ae75c2cf4375c065193b1bb49a9deaee71
Golvan98 <gilvinsz@gmail.com>
2025-02-20 20:11:59 +0800

    restarted from scratch, first section done

--- src/assets/Components/MainPage.jsx
+++ src/assets/Components/MainPage.jsx
@@ -6,54 +6,62 @@ import Footer from './Footer.jsx';
 function MainPage() {
   return (
 
-
-    <main className="fixed inset-0 overflow-y-auto snap-y snap-mandatory bg-deepPurple flex flex-col ">
-     
-      <header className="text-3xl font-bold flex justify-between p-3 border-b  border-b-neonPink">
-      <h1 className="text-lightCyan items-center flex-1 justify-start text-center">Gilvin🧑‍💻</h1>
-
-
-        <h1 className="text-lightCyan flex justify-between items-center w-1/4 justify-center">myHeadSpace ☰</h1>
+    <body className="bg-deepPurple w-full min-h-[100vh] ">
+      
+      <header className="text-3xl h-auto font-bold flex justify-between items-center border-b border-b-neonPink bg-deepPurple mx-16 min-h-[10vh]">
+        <h1 className="text-lightCyan">Gilvin🧑‍💻</h1>
+        <h1 className="text-lightCyan">myHeadSpace ☰</h1>
       </header>
-     
-      <section id="section1" className="h-screen flex-wrap items-center justify-center bg-deepPurple text-white">
-        <div id="section1FirstPartBlankSpace" className="text-4xl text-lightCyan h-1/5"> 
-        </div>
-        
 
-        <article id="section1Container" className={`${classes.bigScreenSection1Container} ${classes.smallScreenSection1Container}`} >
-        
-            <figure id="section1LeftSide" className={`${classes.section1LeftSideBig} ${classes.section1LeftSideSmall} `}>
-              <div className="mt-4 w-full h-1/4">
-              Here to help with everyTech🤓
-              </div>
-              <div className="mt-4 w-full h-3/4"> Hello there, I am Gilvin Zalsos. I am a react developer fixated on delivering websites for anyone in need of my services. This website has everything
-              you need to know (both personal and professional) about me. 
-              </div>
-            </figure>
 
-            <figure id="section1RightSide" className={`${classes.section1RightSideBig} ${classes.section1RightSideSmall}`}>
-              <div className="w-auto h-4/5 rounded-lg">
+
+      <main className="bg-deepPurple text-white min-h-[100vh] flex">
+        
+      
+      <article className="min-h-[90vh] flex justify-between text-xs md:text-2xl">
+
+        <aside className="flex flex-col justify justify-start w-1/2 items-center gap-12">
+
+          <div className="h-1/3 flex items-center justify-center "> 
+          <p className="text-xs md:text-3xl"> Here to help with everyTech🤓 </p>
+          </div>
+
+          <div className="h-auto flex items-center justify-center "> 
+          <p className="mx-2"> Hello there, I am Gilvin Zalsos. I am a react developer fixated on delivering websites for anyone in need of my services. This website has everything
+          you need to know (both personal and professional) about me.  </p>
+          </div>
+
+
+        </aside>
+
+
+        <aside className="w-1/2 h-full flex items-center justify-center ">
+        <div className="w-auto h-1/2 rounded-lg flex items-center justify-center bg-red-300">
               <img src="https://i.imgur.com/OWZxfoV.jpeg" className="object-contain w-full h-full" alt="Example" />  
               </div> 
-               {
-               // <img src="https://i.imgur.com/OWZxfoV.jpeg" className="object-contain w-full h-full" alt="Example" /> 
-               }  
-            </figure>
+        </aside>
 
-        </article>
+        
+      </article>
 
-       
-      </section>  {/* End of Section 1 */}
-
-      <SecondSection/> 
-       <ThirdSection/>
-       {/* <Footer/> */} 
-      
-   
       
 
-    </main>
+        
+      
+
+
+
+      
+
+         {/*<SecondSection/> */}
+         {/* <ThirdSection/> */}
+        {/* <Footer/> */} 
+        
+    
+        
+
+      </main>
+    </body>
   );
 }
 
