2590e4a9c133a3a8f3c3cd23a8136015b3d969ed
Golvan98 <gilvinsz@gmail.com>
2025-04-09 20:05:58 +0800

    added conditional rendering for tasks

--- src/assets/Components/MyHeadSpace.jsx
+++ src/assets/Components/MyHeadSpace.jsx
@@ -13,27 +13,79 @@ function BlankPage(){
       id: 1,
       title: "lores epsm",
       description: "This is a sample task about Roman things.",
-      image: "https://images.pexels.com/photos/3299/postit-scrabble-to-do.jpg?auto=compress&cs=tinysrgb&w=600"
+      image: "https://images.pexels.com/photos/3299/postit-scrabble-to-do.jpg?auto=compress&cs=tinysrgb&w=600",
+      category: "personal"
     },
     {
       id: 2,
       title: "dolores de maximum opus",
       description: "The max power of the opus dei is within.",
-      image: "https://images.pexels.com/photos/414519/pexels-photo-414519.jpeg?auto=compress&cs=tinysrgb&w=600"
+      image: "https://images.pexels.com/photos/414519/pexels-photo-414519.jpeg?auto=compress&cs=tinysrgb&w=600",
+      category: "work"
     },
     {
       id: 3,
       title: "at non este",
       description: "Lorem ipsum dolor sit amet.",
-      image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&w=600"
+      image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&w=600",
+      category: "gaming"
     },
     { id:4,
     title: "bisaya ko nga nasalaag",
     description: "imperialist manila huhuhuhu",
-    image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&w=600"
-    }
+    image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&w=600",
+    category: "others"
+    },
+
+    {
+      id: 5,
+      title: "lores epsm personal 2",
+      description: "This is a sample task about Roman things.",
+      image: "https://images.pexels.com/photos/3299/postit-scrabble-to-do.jpg?auto=compress&cs=tinysrgb&w=600",
+      category: "personal"
+    },
+    {
+      id: 6,
+      title: "dolores de maximum opus work 2",
+      description: "The max power of the opus dei is within.",
+      image: "https://images.pexels.com/photos/414519/pexels-photo-414519.jpeg?auto=compress&cs=tinysrgb&w=600",
+      category: "work"
+    },
+    {
+      id: 7,
+      title: "at non este gaming2",
+      description: "Lorem ipsum dolor sit amet.",
+      image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&w=600",
+      category: "gaming"
+    },
+    { id:8,
+    title: "bisaya ko nga nasalaag others 2",
+    description: "imperialist manila huhuhuhu",
+    image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&w=600",
+    category: "others"
+    },
+
+    { id:9,
+      title: "bisaya ko nga nasalaag others 3",
+      description: "imperialist manila huhuhuhu",
+      image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&w=600",
+      category: "others"
+      },
+
+      { id:10,
+        title: "bisaya ko nga nasalaag others 4",
+        description: "imperialist manila huhuhuhu",
+        image: "https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&w=600",
+        category: "others"
+        },
+
+    
   ];
 
+    const personalTasks = tasks.filter(task => task.category==="personal");
+    const workTasks = tasks.filter(task => task.category==="work");
+    const gamingTasks = tasks.filter(task => task.category==="gaming");
+    const otherTasks = tasks.filter(task => task.category==="others");
 
     const [clickedButton, setClickedButton] =useState(null);
 
@@ -58,23 +110,23 @@ function BlankPage(){
         <main className="flex w-full min-h-[100vh] items-center justify-center">
             <article className='w-4/5 h-4/5 mx-auto flex items-center justify-center bg-inherit '>
                 <aside id="Categories" className={`h-full w-1/5 h-full  items-center justify-start overflow-y-auto bg-[#3498DB]  flex flex-col  rounded-l-lg`}>
-
+     
                   <div className={`mt-10 mb-2 font-bold  ${classes.secondTaskBox}`}> 
-                    <p className="text-white text-md">Categories {clickedButton && `- ${clickedButton}`} </p>
+                    <p className="text-white text-md">Categories  {clickedButton && `- ${clickedButton}`} </p>
                   </div>
                   
                   <section id="categoryContainer" className={`h-full w-full flex-col items-center  justify-center`}> 
 
                     <div className='p-1 text-center bg-inherit  w-full flex items-center justify-center space-y-4'> 
-                      <button onClick={ () =>handleButtonClick("personalProjects")} className='p-1.5 w-2/3 bg-white h-full hover:bg-gray-300 rounded-sm'> Personal Projects</button>
+                      <button onClick={ () =>handleButtonClick("personal")} className='p-1.5 w-2/3 bg-white h-full hover:bg-gray-300 rounded-sm'> Personal Projects</button>
                     </div>
 
                     <div className='p-1 text-center bg-inherit  w-full flex items-center justify-center'> 
-                      <button onClick={ () =>handleButtonClick("workReminders")} className='p-1.5 w-2/3 bg-white hover:bg-gray-300  h-full rounded-sm'> Work Reminders</button>
+                      <button onClick={ () =>handleButtonClick("work")} className='p-1.5 w-2/3 bg-white hover:bg-gray-300  h-full rounded-sm'> Work Reminders</button>
                     </div>
 
                     <div className='p-1 text-center bg-inherit  w-full flex items-center justify-center'> 
-                      <button onClick={ () =>handleButtonClick("gamingStuff")}  className='p-1.5 w-2/3 bg-white hover:bg-gray-300  h-full rounded-sm'> Gaming Stuff</button>
+                      <button onClick={ () =>handleButtonClick("gaming")}  className='p-1.5 w-2/3 bg-white hover:bg-gray-300  h-full rounded-sm'> Gaming Stuff</button>
                     </div>
 
                     <div className='p-1 text-center bg-inherit  w-full flex items-center justify-center'> 
@@ -89,15 +141,24 @@ function BlankPage(){
                 <aside id="nav2" className="h-full w-2/5  flex items-center justify-center text-black bg-[#2980B9]">
                   <div className="w-4/5 rounded-sm h-5/6 flex-wrap justify-between flex items-start justify-center overflow-y-auto ">
 
-                  {
-                  tasks.map(task => (
-                  <button key={task.id} 
-                  onClick={ () => handleTaskClick(task)} 
-                  className={`my-2 bg-white w-2/5 h-10 mx-auto rounded-md flex items-center justify-center break-all px-2 text-center ${classes.secondTaskBox}`} >
-                    {task.title}
-                  </button>
-                  ))
-                  }
+
+                  
+
+                  {personalTasks.map (task => (
+                    <button onClick={ () => handleTaskClick(task)}  key={task.id} className={`my-2 bg-white w-2/5 h-10 mx-auto rounded-md flex items-center justify-center break-all px-2 text-center ${classes.secondTaskBox}`}> {task.title} </button>
+                  ))}
+
+                  {gamingTasks.map (task => (
+                    <button onClick={ () => handleTaskClick(task)}  key={task.id} className={`my-2 bg-white w-2/5 h-10 mx-auto rounded-md flex items-center justify-center break-all px-2 text-center ${classes.secondTaskBox}`}> {task.title} </button>
+                  ))}
+
+                   {otherTasks.map (task => (
+                    <button onClick={ () => handleTaskClick(task)}  key={task.id} className={`my-2 bg-white w-2/5 h-10 mx-auto rounded-md flex items-center justify-center break-all px-2 text-center ${classes.secondTaskBox}`}> {task.title} </button>
+                  ))}
+
+                   {workTasks.map (task => (
+                    <button onClick={ () => handleTaskClick(task)}  key={task.id} className={`my-2 bg-white w-2/5 h-10 mx-auto rounded-md flex items-center justify-center break-all px-2 text-center ${classes.secondTaskBox}`}> {task.title} </button>
+                  ))}
 
                    
                   </div>
